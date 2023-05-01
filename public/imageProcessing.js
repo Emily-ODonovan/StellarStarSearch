daf02a30b6731fb5dc35adecba9e39ea453e4978
Emily <emilyeodonovan@gmail.com>
2023-05-01 20:45:14 +0100

    Constallation UI

--- public/imageProcessing.js
+++ public/imageProcessing.js
@@ -7,13 +7,18 @@ async function loadFromForm() {
 
     const fileIn = document.getElementById("imgUp");
     const imgOut = document.getElementById("out");
+    const starDistance = 10;
+    brightPoints.length = 0;
 
     target = fileIn.files[0];
 
     imgOut.src = URL.createObjectURL(target);
     imgOut.onload = function () {
-        findBrightPoints(imgOut, 5);
+        findBrightPoints(imgOut, starDistance);
         console.log(JSON.stringify(brightPoints, null, "  "));
+        let dimensions = [imgOut.width, imgOut.height];
+        console.log(dimensions[0] + " " + dimensions[1]);
+        drawConstallation(dimensions, brightPoints, 5);
     };
 }
 
@@ -47,10 +52,12 @@ async function findBrightPoints(image, minDistance) {
     // loop through each pixel in the image
     for (let i = 0; i < data.length; i += 4) {
         // get the brightness of the pixel
-        const brightness = (0.2126 * data[i]) + (0.7152 * data[i + 1]) + (0.0722 * data[i + 2]);
+        const brightness = (0.2126 * data[i]) + (0.7152 * data[i + 1]) + (0.073 * data[i + 2]);
+        //these are the RGB brightness weighting given to each values. They were provided from chatGPT and are based on how sensitive the human eye is to each color
+        //the blue value had to be modified to 0.073 from 0.0722 as in testing it was found that the blue value was too low and was not picking up enough stars
 
         // if the pixel is bright, add it to the array
-        if (brightness > 75) {
+        if (brightness > givenBrightness) {
             const x = (i / 4) % width;
             const y = Math.floor((i / 4) / width);
 
@@ -93,4 +100,52 @@ async function findBrightPoints(image, minDistance) {
     }
     console.log("Image Processed!");
     // console.log(JSON.stringify(brightPoints, null, "  "));
+}
+
+function drawConstallation(dimensions, points, radius = 10) {
+    const previewCanvas = document.getElementById("previewCanvas");
+    const ctx = previewCanvas.getContext("2d");
+
+    // Set the canvas dimensions
+    previewCanvas.width = dimensions[0];
+    previewCanvas.height = dimensions[1];
+
+    //background first
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, dimensions[0], dimensions[1]);
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#AAA';
+    const maxLineDistanceInPx = 500;
+
+    // A nested loop function that loops per point for every other point and draws a line between them if they are close enough
+    for (let i = 0; i < points.length; i++) {
+        const start = points[i];
+        for (let j = i + 1; j < points.length; j++) {
+            if (!(i === j)) {
+            const end = points[j];
+
+            // Calculate the distance between the two points
+            const distance = Math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2);
+
+            // Only draw the line if the distance is within the maximum distance
+            if (distance <= maxLineDistanceInPx) {
+                ctx.beginPath();
+                ctx.moveTo(start.x, start.y);
+                ctx.lineTo(end.x, end.y);
+                ctx.stroke();
+            }
+        }
+    }
+
+        // Set the fill style to white
+        ctx.fillStyle = '#fff';
+
+        // Loop through each point and draw a circle
+        for (const point of points) {
+            ctx.beginPath();
+            ctx.arc(point.x, point.y, radius, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    }
 }
